f861f8d147e64bc636acd6fbd7a0802bc719a380
doblefilmina <brusafederico@gmail.com>
2020-10-20 17:37:31 -0300

    i.00) synchronize the database

--- server.js
+++ server.js
@@ -2,15 +2,17 @@
 
 const Hapi = require('@hapi/hapi');
 const Settings = require('./settings');
-const Routes = require('./lib/routes')
+const Routes = require('./lib/routes');
+const Models = require('./lib/models/');
 
 const init = async() => {
   const server = new Hapi.Server({port: Settings.port});
 
   server.route(Routes);
 
+  await Models.sequelize.sync();
   await server.start();
-  console.log(`Server running at: ${server.info.uri}`)
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandleRejection', err => {
