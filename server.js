938fec8f39a09019888e55cd6dba865f186eda34
doblefilmina <brusafederico@gmail.com>
2020-10-26 09:59:17 -0300

    n.00) add support for views on the server

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const Hapi = require('@hapi/hapi');
 const Settings = require('./settings');
 const Routes = require('./lib/routes');
 const Models = require('./lib/models/');
+const Path = require("path");
 
 const init = async() => {
   const server = new Hapi.Server({ port: Settings.port });
 
-  server.route(Routes);
+  await server.register([require("@hapi/vision")]);
 
+  server.views({
+    engines: { pug: require("pug")},
+    path: Path.join(__dirname, "lib/views"),
+    compileOptions: {
+      pretty: false
+    },
+    isCached: Settings.env === "production"
+  });
+
+  //Add routes
+  server.route(Routes);
+  
   await Models.sequelize.sync();
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
